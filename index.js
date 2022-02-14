092a2b8065b5f95abd11e8edbda611c674380aa6
amsg18 <amsg18@alu.ua.es>
2022-02-14 13:29:55 +0100

    Primer API REST con Express

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var http= require('http');
-
-http.createServer((request,response)=> {
-    response.writeHead(200,{'Content.Type':'text/plain'});
-    response.end('Hola a todas y a todos!\n');
-}).listen(8080);
-
-console.log('Servidor ejecutándose en puerto 8080...');
+'use strict' 
+ 
+const express = require('express'); 
+const app = express(); 
+ 
+ 
+app.get('/hola', (request, response) => { 
+response.send('Hola a todas y a todos desde Express!') 
+}); 
+ 
+app.listen(8080, () => { 
+  console.log('API REST ejecutándose en http://localhost:8080/hola'); 
+});
