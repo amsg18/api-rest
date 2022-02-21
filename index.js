84c704633a844e5ea3c5480bf7d7112de103cdd3
amsg18 <amsg18@alu.ua.es>
2022-02-21 15:06:12 +0100

    Incorporamos Nodemon y Morgan

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict' 
  
-const port = process.env.PORT || 8888; 
+const port = process.env.PORT || 3000; 
  
 const express = require('express'); 
+const logger = require('morgan');
 const app = express();
-app.get('/hola/:unNombre', (req, res) => { 
-  res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` }); 
+//Declaracion de los middleware
+app.use(logger('dev'));
+
+
+//Declaracion del API
+app.get('/hola/:name', (req, res) => { 
+  res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD con JSON!` }); 
   }); 
    
   app.listen(port, () => { 
