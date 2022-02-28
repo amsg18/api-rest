9100c8fb20725df371d9661883809fa331198ec5
amsg18 <amsg18@alu.ua.es>
2022-02-28 12:10:38 +0100

    API REST CRUD (sin base de datos)

--- index.js
+++ index.js
@@ -6,14 +6,55 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 //Declaracion de los middleware
-app.use(logger('dev'));
+app.use(logger('dev')); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-
-//Declaracion del API
-app.get('/hola/:name', (req, res) => { 
-  res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD con JSON!` }); 
-  }); 
-   
-  app.listen(port, () => { 
-    console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`); 
+//Declaramos de nuestras rutas y definimos nuestros controladores y lógica de negocio
+app.get('/api/products', getProductController);
+function getProductController(req, res){
+  res.status(200).send({
+    msg:"Ahí están todos los productos",
+    productos: []
   });
+};
+
+app.get('/api/products/:productID', (req,res)=>{
+  const id= req.params.productID;
+
+  res.status(200).send({
+    msg:`Ahí va el producto ${id} solicitado`,
+    "ID del producto": id
+  });
+});
+
+app.post('/api/products', (req, res) => {
+  const miProducto=req.body;
+  console.log(miProducto);
+  res.status(200).send({
+    mmsg:"He creado un nuevo producto",
+    producto:miProducto
+  });
+});
+
+app.put('/api/products/:productID', (req, res) => {
+  const ID = req.params.productID;
+  const miProducto= req.body;
+  res.status(200).send({
+      _id:`${ID}`,
+      product: miProducto
+  });
+});
+
+app.delete('/api/products/:productID', (req, res) => {
+  const ID = req.params.productID;
+  res.status(200).send({
+      result: 'OK',
+      _id: `${ID}`
+  });
+});
+
+  app.listen(port, () => { 
+    console.log(`API REST CRUD ejecutándose en http://localhost:${port}/api/products`); 
+  });
+
